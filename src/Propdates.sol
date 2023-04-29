3ace08380efbd90ef90d3a8042e936697c0e7350
Wilson Cusack <cusackw@gmail.com>
2023-04-29 18:03:03 -0400

    started

--- src/Propdates.sol
+++ src/Propdates.sol
@@ -1,36 +1,54 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Proposal} from 'lib/nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOInterface.sol';
+import {NounsDAOLogicV2} from "lib/nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOLogicV2.sol";
+import "lib/nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOInterfaces.sol";
 
 contract Propdates {
-    mapping (uint256 => address) public propUpdateAdmin;
-    mapping (uint256 => address) public pendingPropUpdateAdmin;
+    address payable public constant NOUNS_DAO = payable(0x6f3E6272A167e8AcCb32072d08E0957F9c79223d);
+
+    mapping(uint256 => address) public propUpdateAdmin;
+    mapping(uint256 => address) public pendingPropUpdateAdmin;
 
     event PropUpdateAdminTransferStarted(uint256 indexed propId, address indexed oldAdmin, address indexed newAdmin);
     event PropUpdateAdminTransfered(uint256 indexed propId, address indexed oldAdmin, address indexed newAdmin);
+    event PostUpdate(uint256 indexed propId, string update);
 
-    modifier onlyPropUpdateAdmin(uint256 _propId) {
-        require(msg.sender == propUpdateAdmin[_propId]);
+    error OnlyPropUpdateAdmin();
+
+    modifier onlyPropUpdateAdmin(uint256 propId) {
+        if(msg.sender != propUpdateAdmin[propId]) {
+            revert OnlyPropUpdateAdmin();
+        }
         _;
     }
 
-    function transferPropUpdateAdminPower(uint256 _propId, address _newAdmin) public  {
-        require(msg.sender == propUpdateAdmin[_propId] || );
-        pendingPropUpdateAdmin[_propId] = _newAdmin;
+    function transferPropUpdateAdminPower(uint256 propId, address _newAdmin) external {
+        address currentAdmin = propUpdateAdmin[propId];
+        if (
+            msg.sender != currentAdmin
+                && !(currentAdmin == address(0) && NounsDAOLogicV2(NOUNS_DAO).proposals(propId).proposer == msg.sender)
+        ) {
+            revert OnlyPropUpdateAdmin();
+        }
+        pendingPropUpdateAdmin[propId] = _newAdmin;
 
-        emit PropUpdateAdminTransferStarted(_propId, msg.sender, _newAdmin);
+        emit PropUpdateAdminTransferStarted(propId, currentAdmin, _newAdmin);
     }
 
-    function acceptPropUpdateAdminPower(uint256 _propId) public {
-        require(msg.sender == pendingPropUpdateAdmin[_propId]);
+    function acceptPropUpdateAdminPower(uint256 propId) external {
+        require(msg.sender == pendingPropUpdateAdmin[propId]);
 
-        address oldAdmin = propUpdateAdmin[_propId];
-        address newAdmin = pendingPropUpdateAdmin[_propId];
+        address oldAdmin = propUpdateAdmin[propId];
+        address newAdmin = pendingPropUpdateAdmin[propId];
 
-        propUpdateAdmin[_propId] = newAdmin;
-        pendingPropUpdateAdmin[_propId] = address(0);
+        propUpdateAdmin[propId] = newAdmin;
+        pendingPropUpdateAdmin[propId] = address(0);
 
-        emit PropUpdateAdminTransfered(_propId, oldAdmin, newAdmin);
+        emit PropUpdateAdminTransfered(propId, oldAdmin, newAdmin);
+    }
+
+    function postUpdate(uint256 propId, string calldata update) onlyPropUpdateAdmin(propId) external {
+        emit PostUpdate(propId, update);
     }
 }
