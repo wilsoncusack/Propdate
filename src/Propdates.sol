bb0798d7f3a286d545858d6351cae4b3db2f2429
Wilson Cusack <cusackw@gmail.com>
2023-05-19 18:39:36 -0400

    save timestamp of last updated

--- src/Propdates.sol
+++ src/Propdates.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
 import {NounsDAOLogicV2} from "lib/nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOLogicV2.sol";
 import "lib/nouns-monorepo/packages/nouns-contracts/contracts/governance/NounsDAOInterfaces.sol";
 
 contract Propdates {
+    struct PropdateInfo {
+        address propUpdateAdmin;
+        uint96 lastUpdated;
+    }
+    
     address payable public constant NOUNS_DAO = payable(0x6f3E6272A167e8AcCb32072d08E0957F9c79223d);
 
-    mapping(uint256 => address) public propUpdateAdmin;
+    mapping(uint256 => PropdateInfo) public propdateInfo;
     mapping(uint256 => address) public pendingPropUpdateAdmin;
 
     event PropUpdateAdminTransferStarted(uint256 indexed propId, address indexed oldAdmin, address indexed newAdmin);
@@ -17,15 +22,8 @@ contract Propdates {
     error OnlyPropUpdateAdmin();
     error OnlyPendingPropUpdateAdmin();
 
-    modifier onlyPropUpdateAdmin(uint256 propId) {
-        if(msg.sender != propUpdateAdmin[propId]) {
-            revert OnlyPropUpdateAdmin();
-        }
-        _;
-    }
-
     function transferPropUpdateAdminPower(uint256 propId, address newAdmin) external {
-        address currentAdmin = propUpdateAdmin[propId];
+        address currentAdmin = propdateInfo[propId].propUpdateAdmin;
         if (
             msg.sender != currentAdmin
                 && !(currentAdmin == address(0) && NounsDAOLogicV2(NOUNS_DAO).proposals(propId).proposer == msg.sender)
@@ -38,20 +36,32 @@ contract Propdates {
     }
 
     function acceptPropUpdateAdminPower(uint256 propId) external {
-        if(msg.sender != pendingPropUpdateAdmin[propId]) {
+        if (msg.sender != pendingPropUpdateAdmin[propId]) {
             revert OnlyPendingPropUpdateAdmin();
         }
 
-        delete pendingPropUpdateAdmin[propId];
-
-        address oldAdmin = propUpdateAdmin[propId];
-        propUpdateAdmin[propId] = msg.sender;
-
-        emit PropUpdateAdminTransfered(propId, oldAdmin, msg.sender);
+        _acceptPropUpdateAdminPower(propId);
     }
 
-    /// NOTE we could restrict this to successfully funded props only, but maybe fine/interesting to leave open?
-    function postUpdate(uint256 propId, string calldata update) onlyPropUpdateAdmin(propId) external {
+    function postUpdate(uint256 propId, string calldata update) external {
+        if (msg.sender != propdateInfo[propId].propUpdateAdmin) {
+            if (msg.sender == pendingPropUpdateAdmin[propId]) {
+                _acceptPropUpdateAdminPower(propId);
+            } else {
+                revert OnlyPropUpdateAdmin();
+            }
+        }
+
+        propdateInfo[propId].lastUpdated = uint96(block.timestamp);
         emit PostUpdate(propId, update);
+    }
+
+    function _acceptPropUpdateAdminPower(uint256 propId) internal {
+        delete pendingPropUpdateAdmin[propId];
+
+        address oldAdmin = propdateInfo[propId].propUpdateAdmin;
+        propdateInfo[propId].propUpdateAdmin = msg.sender;
+
+        emit PropUpdateAdminTransfered(propId, oldAdmin, msg.sender);
     }
 }
