22cde8d38aeb609e09de35b0b65a887519b6d547
Wilson Cusack <cusackw@gmail.com>
2023-06-18 22:26:54 -0400

    started on gas refund

---
+++ src/GasRefund.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.4;
+
+library GasRefund {
+    /// @notice The maximum priority fee used to cap gas refunds in `castRefundableVote`
+    uint256 public constant MAX_REFUND_PRIORITY_FEE = 2 gwei;
+
+    /// @notice The vote refund gas overhead, including 7K for ETH transfer and 29K for general transaction overhead
+    uint256 public constant REFUND_BASE_GAS = 36000;
+
+    /// @notice The maximum gas units the DAO will refund voters on; supports about 9,190 characters
+    uint256 public constant MAX_REFUND_GAS_USED = 200_000;
+
+    /// @notice The maximum basefee the DAO will refund voters on
+    uint256 public constant MAX_REFUND_BASE_FEE = 200 gwei;
+
+    // from https://github.com/nounsDAO/nouns-monorepo/blob/master/packages/nouns-contracts/contracts/governance/NounsDAOLogicV2.sol#LL1033C4-L1033C4
+    function _refundGas(uint256 startGas) internal {
+        unchecked {
+            uint256 balance = address(this).balance;
+            if (balance == 0) {
+                return;
+            }
+            uint256 basefee = min(block.basefee, MAX_REFUND_BASE_FEE);
+            uint256 gasPrice = min(tx.gasprice, basefee + MAX_REFUND_PRIORITY_FEE);
+            uint256 gasUsed = min(startGas - gasleft() + REFUND_BASE_GAS, MAX_REFUND_GAS_USED);
+            uint256 refundAmount = min(gasPrice * gasUsed, balance);
+            tx.origin.call{ value: refundAmount }('');
+        }
+    }
+
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+}
